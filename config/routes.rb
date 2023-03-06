7fb3a76a5a94ef17d015db497dd72c29f3356217
Yuqing Wang <yuqingwang42@gmail.com>
2023-03-06 15:48:53 +0000

    refactored with link_to and url helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # GET /about
-  get 'about', to: 'about#index'
+  get 'about_us', to: 'about#index', as: 'about'
+  # get  "about_us"  => "about#index",  :as => 'about'
 
   root to: 'main#index'
 end
