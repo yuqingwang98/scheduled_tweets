0490c2da434a08054c4db6db1a4b1c111caa8e97
Yuqing Wang <yuqingwang42@gmail.com>
2023-03-07 23:44:15 +0000

    create sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # GET /about
   get 'about_us', to: 'about#index', as: 'about'
   # get  "about_us"  => "about#index",  :as => 'about'
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
 
   root to: 'main#index'
 end
